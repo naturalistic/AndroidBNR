9e51a34be5c1eeb75068cebec29cc39e72c7d35d
naturalistic <daniel.r.roberts@gmail.com>
2015-12-03 17:49:29 +0000

    Created criminal intent project

--- CriminalIntent/app/src/main/java/com/devdaniel/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/devdaniel/criminalintent/CrimeActivity.java
@@ -1,38 +1,36 @@
 package com.devdaniel.criminalintent;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class CrimeActivity extends FragmentActivity {
 
-public class CrimeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_crime, menu);
-        return true;
-    }
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
